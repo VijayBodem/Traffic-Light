7cfb9972a406b4c39a18aeeef6fc4303d4fa44ed
VijayBodem <128890613+VijayBodem@users.noreply.github.com>
2023-05-02 14:37:53 +0530

    Traffic Light

---
+++ traffic.js
@@ -0,0 +1,28 @@
+function redButton() {
+    stopButton.style.backgroundColor = "red";
+    goButton.style.backgroundColor = "black";
+    readyButton.style.backgroundColor = "black";
+    stopLight.style.backgroundColor = "red";
+    readyLight.style.backgroundColor = "#4b5069";
+    goLight.style.backgroundColor = "#4b5069";
+
+}
+
+function yellowButton() {
+    readyButton.style.backgroundColor = "yellow";
+    goButton.style.backgroundColor = "black";
+    stopButton.style.backgroundColor = "black";
+    readyLight.style.backgroundColor = "yellow";
+    stopLight.style.backgroundColor = "#4b5069";
+    goLight.style.backgroundColor = "#4b5069";
+
+}
+
+function greenButton() {
+    goButton.style.backgroundColor = "green";
+    readyButton.style.backgroundColor = "black";
+    stopButton.style.backgroundColor = "black";
+    goLight.style.backgroundColor = "green";
+    readyLight.style.backgroundColor = "#4b5069";
+    stopLight.style.backgroundColor = "#4b5069";
+}
